8088538b2e212a351095d7cc8c5604cdbaf2e594
tineodev <tineo.dev@gmail.com>
2022-12-19 10:05:28 -0500

    Rename table

--- api_services/models.py
+++ api_services/models.py
@@ -6,3 +6,6 @@ class Services(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     logo = models.URLField(max_length=255)
+
+    class Meta:
+        db_table = 'Api-Services'
