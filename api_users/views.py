f2d67d6657f563302df78f477ba859027d7cd45d
tineodev <tineo.dev@gmail.com>
2022-12-18 19:12:38 -0500

    Create api_user_forms

--- api_users/views.py
+++ api_users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ApiRegister
+from django.views.generic import CreateView
 
 # Create your views here.
+
+class SignupCreateView(CreateView):
+    form_class = ApiRegister
+    template_name = 'registration/register.html'
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('login')
