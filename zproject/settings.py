212f5f629bcdfa8d87a09a1698b9959f572f6cdc
tineodev <tineo.dev@gmail.com>
2022-12-29 14:51:11 -0500

    Deploy

--- zproject/settings.py
+++ zproject/settings.py
@@ -92,25 +92,25 @@ WSGI_APPLICATION = 'zproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {  
-#     'default': {  
-#         'ENGINE': 'django.db.backends.mysql',  
-#         'NAME': 'railway',  
-#         'USER': 'root',  
-#         'PASSWORD': 'YpgllmllIHZfi9sjfRNn',  
-#         'HOST': 'containers-us-west-44.railway.app',  
-#         'PORT': '6145',  
-#         'OPTIONS': {  
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-#         }  
-#     }  
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db2.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'railway',  
+        'USER': 'root',  
+        'PASSWORD': 'yTyd1MDSbhuzzPtCXJu5',  
+        'HOST': 'containers-us-west-56.railway.app',  
+        'PORT': '7176',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db2.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
