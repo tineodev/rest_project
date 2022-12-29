7f4b7c1f6bbf246bf970b478e55c763ff4a58503
tineodev <tineo.dev@gmail.com>
2022-12-29 15:13:55 -0500

    Fix router backend 2

--- zproject/settings.py
+++ zproject/settings.py
@@ -188,7 +188,9 @@ SIMPLE_JWT = {
 # * Cors
 CORS_ALLOWED_ORIGINS = [
 	'http://127.0.0.1:5500',
+    'https://tineodev.github.io'
 ]
 CORS_ORIGIN_WHITELIST = [
 	'http://127.0.0.1:5500',
+    'https://tineodev.github.io'
 ]
